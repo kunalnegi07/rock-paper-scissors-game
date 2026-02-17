aec16e30109954170ba9a47c7be6dc46c9d7018a
kunalnegi07 <kunal82857@gmail.com>
2026-02-17 22:47:03 +0530

    add ui

--- script.js
+++ script.js
@@ -1,18 +1,27 @@
 let humanScore = 0;
 let computerScore = 0;
-let computerPick= getComputerChoice();
-let roundInProgress = false;
+const playAgain = document.querySelector("#play-again");
+playAgain.addEventListener("click",resetGame);
+
+
+const allbtns = document.querySelectorAll(".buttons");
+
+const gameOverScreen = document.querySelector("#game-over-screen");
+
+const mainScreen = document.querySelector(".mainscreen");
 
 
 function getComputerChoice()
 {
 
-    if(getRandomInt(3) === 0)
+    const random =  getRandomInt(3)
+
+    if(random === 0)
     {
         return "rock" ;
     }
 
-    else if (getRandomInt(3) === 1)
+    else if (random === 1)
     {
         return "paper";
     }
@@ -75,25 +84,57 @@ function declareResult()
 {
     if(humanScore == 5 ){
         showResult("You won the game!")
+        for(const button of allbtns ){
+            button.disabled = true;
+        }
+        gameOver();
     }else if(computerScore == 5){
         showResult("You lost the game!")
+        for(const btns of allbtns ){
+            btns.disabled = true;
+        }
+        gameOver();
     }
     
+    
 }
+
+function gameOver(){
+    gameOverScreen.style.display = "block";
+    mainScreen.style.display = "none";
+}
+
+function resetGame(){
+    humanScore = 0;
+    computerScore = 0;
+    gameOverScreen.style.display = "none";
+    mainScreen.style.display = "block";
+    for(const btns of allbtns ){
+            btns.disabled = false;
+        }
+    result.textContent = "";
+    score.textContent = "";
+    outcome.textContent = "";
+}
+
+
 
 let btn1 = document.querySelector(".rock");
 let btn2 = document.querySelector(".paper");
 let btn3 = document.querySelector(".scissors");
 
 btn1.addEventListener("click",() => {
+    let computerPick= getComputerChoice();
     playRound("rock",computerPick);
     declareResult();
 });
 btn2.addEventListener("click",() => {
+    let computerPick= getComputerChoice();
     playRound("paper",computerPick);
-    declareResult;
+    declareResult();
 });
 btn3.addEventListener("click",() => {
+    let computerPick= getComputerChoice();
     playRound("scissors",computerPick);
     declareResult();
 });
@@ -104,11 +145,14 @@ const outcome = document.querySelector("#outcome")
 
 function showResult(message){
     result.textContent = message;
+    result.style.color = "blue";
 }
 function showScore(message){
     score.textContent = message;
+    result.style.color = "blue";
 }
 function showOutcome(message){
     outcome.textContent = message;
+    result.style.color = "blue";
 }
 
